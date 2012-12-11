ecfcc0d2e9a4867dcaa4eaf3b4e626efbcfe4dda
Max Schaefer <mschaefer@us.ibm.com>
2012-12-10 21:30:07 -0500

    39 failures.

---
+++ test/data/cond.js
@@ -0,0 +1,1 @@
+var x = window === this ? 23 : 42;
---
+++ test/data/normalized.cond.js
@@ -0,0 +1,20 @@
+(function(__global) {
+    var tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6;
+    tmp5 = "window";
+    tmp3 = __global[tmp5];
+    tmp4 = __global;
+    tmp2 = tmp3 === tmp4;
+    tmp6 = tmp2;
+    if (tmp2) {
+        tmp1 = 23;
+    } else {
+        {}
+    }
+    if (tmp6) {
+        {}
+    } else {
+        tmp1 = 42;
+    }
+    tmp0 = "x";
+    __global[tmp0] = tmp1;
+})(this);
